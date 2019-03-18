c25397e3297e678d3b37d3c4204db2c05d3f77bd
Paul Martinetti <paul@digscitech.com>
2019-03-18 17:10:10 -0400

    Added bootstrap css

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.set('views', path.join(__dirname, 'views'))
 // use hogan html template engine
 app.set('view engine', 'hjs')
 
+// public assets
+// specifying the public assets here, so no need to use word public
+app.use(express.static(path.join(__dirname, 'public')))
+
 // production port is usually 80
 app.listen(5000)
 console.log('server running on localhost:5000')
